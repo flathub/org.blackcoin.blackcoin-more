8da8651f57480defc16b819e5ebc63de92360cd1
expatjedi <kbicer@proton.me>
2023-01-07 04:14:05 +0300

    fix data directory

--- install_wrapper.sh
+++ install_wrapper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 echo '#!/usr/bin/bash' > /app/bin/blackmore-qt
-echo "exec /app/bin/internal/blackmore-qt" '-datadir="${XDG_DATA_HOME}" "$@"' >> /app/bin/blackmore-qt
+echo "exec /app/bin/blackmore-qt" '-datadir="${XDG_DATA_HOME}" "$@"' >> /app/bin/blackmore-qt
 chmod 744 /app/bin/blackmore-qt
